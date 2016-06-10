465f30d505ecee07e39e049b9ba05c54b66c222b
Richard Chow <rchow@chef-workstation.mitns.com>
2016-06-09 23:26:12 -0700

    adding sample chef cookbooks

--- cookbooks/awesome_customers_ubuntu/metadata.rb
+++ cookbooks/awesome_customers_ubuntu/metadata.rb
@@ -5,3 +5,7 @@ license 'all_rights'
 description 'Installs/Configures awesome_customers_ubuntu'
 long_description 'Installs/Configures awesome_customers_ubuntu'
 version '0.1.0'
+
+depends 'apt', '~> 4.0.0'
+depends 'firewall', '~> 2.4.0'
+depends 'httpd', '~> 0.3.5'
